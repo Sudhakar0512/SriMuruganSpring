88586aa2a9b03d38c9ca6c00d2cd6320f4ef36dd
Sudhakar0512 <sudhakar.s2021csbs@sece.ac.in>
2023-11-12 13:44:20 +0530

    DockerUpdate

--- SriMuruganServerfinal/src/main/java/com/dev/SriMuruganServerfinalApplication.java
+++ SriMuruganServerfinal/src/main/java/com/dev/SriMuruganServerfinalApplication.java
@@ -2,7 +2,7 @@ package com.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
